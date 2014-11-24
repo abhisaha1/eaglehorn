5c8579ef2ce9d8f5e38993396ca0677c3118bf47
Abhishek Saha <abhisheksaha11@gmail.com>
2014-11-24 20:47:23 +0530

    Model bug fixed

--- application/controller/Welcome.php
+++ application/controller/Welcome.php
@@ -13,16 +13,12 @@ class Welcome extends Base
 
     function index()
     {
-
         //pushing data to template
         $data = array(
             'TITLE' => 'Welcome to Eaglehorn',
             'CONTENT' => 'Welcome.php',
             'HEADER' => ' Get Started !'
         );
-
-        $m = $this->load->model('WelcomeModel');
-        $m->updateme();
 
         $template = $this->load->template('one_col', $data);
         $template->injectCSS(configItem('site')['viewurl'].'style.css');
