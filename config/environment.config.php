1b2c600c01eee6ae699e7096530222cc6e1ae218
Abhishek Saha <abhisheksaha11@gmail.com>
2015-07-14 15:47:09 +0200

    Added enviroment config file

---
+++ config/environment.config.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The Site URL will automatically be changed to the appropriate url
+ * depending on the environment it is working on.
+ */
+$config['environment'] = array(
+
+        'dev' => 'http://localhost/eaglehorn_documentation/public/',
+        'test' => 'http://test.eaglehorn/',
+        'production' => 'http://eaglehorn.org/'
+
+);
