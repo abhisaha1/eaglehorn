b8fc790a15009c320ea4e325fef33d90d473cc93
Abhishek Saha <abhisheksaha11@gmail.com>
2015-07-19 11:53:07 +0200

    Added more hooks

--- config/default/hooks.config.php
+++ config/default/hooks.config.php
@@ -1,28 +1,46 @@
 <?php
-
 $config['hooks'] = array(
 
-        'logger' => array(
-            'class'         => 'logger',
-            'namespace'     => 'hooks\logger',
-            'active'        => 1
-        ),
+    'logger' => array(
+        'class'         => 'logger',
+        'namespace'     => 'hooks\logger',
+        'active'        => 0
+    ),
 
-        'pre_controller' => array(
-            'class'         => 'PreController',
-            'namespace'     => 'hooks',
-            'method'        => 'preload',
-            'active'        => 1
-        ),
+    'pre_controller' => array(
+        'class'         => 'Hooks',
+        'namespace'     => 'hooks',
+        'method'        => 'preload',
+        'active'        => 0
+    ),
 
-        'post_controller' => array(
-            'class'         => 'PreController',
-            'namespace'     => 'hooks',
-            'method'        => 'postload',
-            'active'        => 1
-        ),
+    'post_controller' => array(
+        'class'         => 'Hooks',
+        'namespace'     => 'hooks',
+        'method'        => 'postload',
+        'active'        => 0
+    ),
 
+    'pre_template'  => array(
 
+        'class'         => 'Hooks',
+        'namespace'     => 'hooks',
+        'method'        => 'pretemplate',
+        'active'        => 0
 
+    ),
 
+    'error' => array(
+        'class'         => 'Hooks',
+        'namespace'     => 'hooks',
+        'method'        => 'error',
+        'active'        => 0
+    ),
+
+    '404' => array(
+        'class'         => 'Hooks',
+        'namespace'     => 'hooks',
+        'method'        => 'notFound',
+        'active'        => 0
+    )
 );
